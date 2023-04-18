08477561962fbaccf48584360fac382ef4835580
juanselenu <131208292+juanselenu@users.noreply.github.com>
2023-04-18 19:27:54 -0300

    3

---
+++ Ejercicio Clases/Practica For/Practica For/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+Console.WriteLine("ingrese un numero");
+string numero  = Console.ReadLine();    
+int numeroe = int.Parse(numero);
+int suma = 0;
+
+for (int i = 0; i < 5; numeroe++) ;
+{
+    suma = suma + numeroe;
+}
+
+Console.WriteLine("la suma es " + suma);    
+*/
+
+int suma = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Ingrese el número:" + i);
+    int numero = int.Parse(Console.ReadLine());
+    suma += numero;
+}
+
+Console.WriteLine("La suma de los 5 números es:"+ suma);
---
+++ Ejercicio Clases/Rpaso 1 while solo/Rpaso 1 while solo/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+Console.WriteLine("ingrese un numero:");    
+string numero = Console.ReadLine();
+int numeroe = int.Parse(numero);
+int suma = 0;
+
+while (numeroe > 0)
+{
+
+    if (numeroe < 0)
+    {
+        Console.WriteLine("ingresa un numero positivo");
+    }
+    else
+        suma = suma+numeroe;
+    }
+
+Console.WriteLine("el total es:" + suma);
+*/
+
+Console.WriteLine("ingrese un numero:");
+string numero = Console.ReadLine();
+int numeroe = int.Parse(numero);
+int suma = 0;
+
+while (numeroe > 0) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
