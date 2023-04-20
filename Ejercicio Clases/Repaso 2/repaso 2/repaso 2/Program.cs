04a6c43a7fc0810e62bbbe3789e5329d48511b72
juanselenu <131208292+juanselenu@users.noreply.github.com>
2023-04-20 18:31:59 -0300

    lis

--- Ejercicio Clases/Repaso 2/repaso 2/repaso 2/Program.cs
+++ Ejercicio Clases/Repaso 2/repaso 2/repaso 2/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// pedir 5 numeros, guardarlos en una lista de int y mostrar el promedio 
+List<int> numero = new List<int>();
+
+int suma = 0;
+int prom = 0;
+int total = 0;
+Console.WriteLine("ingrese un numero");
+
+List<int> numero = new List<int>(); 
+if (numero.Count >= 5 );
+{
+    prom++;
+  suma = suma + numero.Count;   
+}
+
+total = prom/ numero.Count;
+
+
+
+
+
+
